2b7687db2802b64e70c08a68fce7928419d775f9
Adonay <adonaypichardo@gmail.com>
2023-05-27 19:43:15 -0700

    can interpret from a file

--- cado++/src/runFile.cpp
+++ cado++/src/runFile.cpp
@@ -1,18 +1,23 @@
+#pragma once
 // Standard Libraries
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 // Non-Standard Libraries
+#include "../inc/common.h"
+#include "errorhandling.cpp"
 #include "run.cpp"
 
 using namespace std;
 
 void runFile(std::string filePath){
     ifstream inputFile(filePath, ios::binary);
+
     if (inputFile){
         string inputStream((istreambuf_iterator<char>(inputFile)), istreambuf_iterator<char>());
         run(inputStream);
+        if (ErrorHandling::hadError) exit(LEXICAL_ERROR);
     }else{
         cout << "[x] Failed to open file." << endl;
     }
