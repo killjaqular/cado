18724ddda25a04ce4bf878bc3db78f830bd1c350
Adonay <adonaypichardo@gmail.com>
2023-05-27 19:42:22 -0700

    can now scan input and generate tokens

--- cado++/src/run.cpp
+++ cado++/src/run.cpp
@@ -1,10 +1,21 @@
+#pragma once
 // Standard Libraries
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <list>
+
+// Non-Standard Libraries
+#include "scanner.cpp"
 
 using namespace std;
 
 void run(string& inputStream){
+    Scanner scanner = Scanner(inputStream);
+    list<Token> tokens = scanner.scanTokens();
+
+    for (Token token : tokens){
+        // cout << token; // TODO: override the << operator in class Token
+    }
 
     return;
 }
